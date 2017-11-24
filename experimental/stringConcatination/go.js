4a1814595fd2aaae01491d0e88ca0d3486462116
timhaag <tim.haag@timhaag.de>
2017-11-24 12:51:39 +0100

    Added an example about the new string concatination in ES6

---
+++ experimental/stringConcatination/go.js
@@ -0,0 +1,10 @@
+let stringOne = 'Hello World!'
+let stringTwo = 'Tim'
+let stringThree = 'Engineer'
+
+let outputOne = 'I\'m ' + stringTwo + ' and I\'m a ' + stringThree + '! ' + stringOne
+
+let outputTwo = `I'm ${stringTwo} and I'm a ${stringThree}! ${stringOne}`
+
+console.log(outputOne);
+console.log(outputTwo);
